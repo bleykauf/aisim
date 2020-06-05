corrected formula in density_matrices method of AtomicEnsemble

aisim/atoms.py
@@ -93,7 +93,8 @@ class AtomicEnsemble():
         n × m x m array, representing the density matrix of the m level system of the n atoms.
         These are pure states.
         """
-        return np.einsum('ij,il->ijl', self.state_vectors, self.state_vectors)
+        # |Psi><Psi|
+        return np.einsum('ji,il->ijl', np.conjugate(self.state_vectors).T, self.state_vectors)
 
     @property
     def density_matrix(self):
@@ -102,6 +103,7 @@ class AtomicEnsemble():
         """
         pure_dm = self.density_matrices
         N_atoms = self.state_vectors.shape[0]
+        # sum over pure |Psi><Psi| and divide by N
         return 1/N_atoms * np.einsum('ijk->jk', pure_dm) # sum over pure state's density matrices
 
     @property