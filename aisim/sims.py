a39ee8fe75acc0aad05727666dd699ed2228f58e
Bastian Leykauf <leykauf@physik.hu-berlin.de>
2020-06-11 17:23:21 +0200

    no more flake8 warnings

--- aisim/sims.py
+++ aisim/sims.py
@@ -1,13 +1,11 @@
-"""High-level functions for simulating certain aspects of atom interferometers."""
+"""Functions for simulating certain aspects of atom interferometers."""
 
 import numpy as np
-from . import convert
 
 
 def wavefront_simulation(t, wf, atoms, det):
     """
-    Simulation of the phase or gravity bias caused by wavefront aberrations in a Mach-Zehnder atom
-    interferomter.
+    Calculate the phase or gravity bias caused by wavfront abberrations.
 
     Parameters
     ----------
@@ -23,23 +21,22 @@ def wavefront_simulation(t, wf, atoms, det):
     Returns
     -------
     weighted_awf : float
-        Complex amplitude factor from which contrast and phase shift can be determined via `abs()` 
-        and `np.angle()`
+        Complex amplitude factor from which contrast and phase shift can be
+        determined via `abs()` and `np.angle()`
     """
-
     # unpack the times of detection and the interferometer pulses
     t1, t2, t3 = t
 
     atoms = det.detected_atoms(atoms)
 
-    # calculate the imprinted phase for each "test atom" at each pulse. This is the computationally
-    #  heavy part
+    # calculate the imprinted phase for each "test atom" at each pulse. This is
+    # the computationally heavy part
     phi1 = wf.get_value(atoms.calc_position(t1))
     phi2 = wf.get_value(atoms.calc_position(t2))
     phi3 = wf.get_value(atoms.calc_position(t3))
 
-    # calculate a complex amplitude factor for the Mach-Zehnder sequence and weight their
-    # contribution to the signal
+    # calculate a complex amplitude factor for the Mach-Zehnder sequence and
+    # weight their contribution to the signal
     awf = np.exp(1j * 2 * np.pi * (phi1 - 2*phi2 + phi3))
     weighted_awf = np.sum(atoms.weights * awf) / np.sum(atoms.weights)
 
--- tests/convert_test.py
+++ tests/convert_test.py
@@ -1,16 +1,19 @@
 import pytest
 import aisim as ais
 
+
 def test_mass():
     for mass in ais.mass.values():
         assert mass > 0
- 
+
+
 def test_temp():
     for species in ais.mass:
         assert ais.temp(0, species=species) == 0
         assert ais.temp(1.0, species=species) > 0
         with pytest.raises(ValueError):
             ais.temp(-1.0, species=species)
+
 
 def test_vel_from_temp():
     for species in ais.mass:
