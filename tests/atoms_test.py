798246831490397b0caeaf8ba54aa09c1524d9b0
Sascha Vowe <vowe@mail.de>
2020-06-11 14:55:59 +0200

    added more tests of methods of AtomicEnsemble

--- tests/atoms_test.py
+++ tests/atoms_test.py
@@ -12,28 +12,59 @@ def test_AtomicEnsemble_methods():
         state_kets, state_bras, density_matrices,
         density_matrix
     '''
-    N_atom = 1000
-    N_ints = [2, 3, 5, 10, 20, 100]
-    for N_int in N_ints:
-        # Generate random states
-        random_phase_space_vectors = np.random.rand(N_atom, 6)
-        random_kets = np.random.rand(N_atom, N_int)
+    def atomic_ensemble_test_function(atoms):
+        # Testing AtomicEnsemble properties
+        assert atoms.state_kets.shape == (n_atom, n_int, 1)
+        assert atoms.state_bras.shape == (n_atom, 1, n_int)
+        assert atoms.phase_space_vectors.shape == (n_atom, 6)
+        assert atoms.phase_space_vectors.shape == (n_atom, 6)
+        assert (atoms.phase_space_vectors[:,0:3] == atoms.position).all()
+        assert (atoms.calc_position(0) == atoms.position).all()
+        assert (atoms.phase_space_vectors[:,3:6] == atoms.velocity).all()
+        # Test duality of Kets and Bras
+        np.testing.assert_array_almost_equal(atoms.state_kets, np.conjugate(
+            np.transpose(atoms.state_bras, (0, 2, 1))))
+        # Test whether the trace of every density matrix is equal to one
+        np.testing.assert_almost_equal(
+            np.trace(atoms.density_matrices, axis1=1, axis2=2), 1)
+        # Test whether every single atoms density matrix satisfies rho^2 = rho (condition for pure states)
+        np.testing.assert_array_almost_equal(np.matmul(
+            atoms.density_matrices, atoms.density_matrices), atoms.density_matrices)
+
+    # Test AtomicEnsemble from very general randomly generated states
+    n_atom = 1000
+    n_ints = [2, 3, 5, 10, 20, 100]
+    for n_int in n_ints:
+        random_phase_space_vectors = np.random.rand(n_atom, 6)
+        random_kets = np.random.rand(n_atom, n_int)
         # normalize random ket
         norm = np.einsum('ij,ij->i', random_kets, random_kets)
         norm_random_kets = np.einsum('ij,i->ij', random_kets, 1/norm**(1/2))
         # apply random phase shifts
-        random_phases = 2*np.pi*np.random.rand(N_atom, N_int)
+        random_phases = 2*np.pi*np.random.rand(n_atom, n_int)
         norm_random_kets = np.exp(1j*random_phases) * norm_random_kets
-        norm_random_kets = np.reshape(norm_random_kets, (N_atom, N_int, 1))
+        norm_random_kets = np.reshape(norm_random_kets, (n_atom, n_int, 1))
         random_atoms = ais.AtomicEnsemble(
             random_phase_space_vectors, norm_random_kets)
-        # Testing AtomicEnsemble properties
-        # Test duality of Kets and Bras
-        np.testing.assert_array_almost_equal(random_atoms.state_kets, np.conjugate(
-            np.transpose(random_atoms.state_bras, (0, 2, 1))))
-        # Test whether the trace of every density matrix is equal to one
-        np.testing.assert_almost_equal(
-            np.trace(random_atoms.density_matrices, axis1=1, axis2=2), 1)
-        # Test whether every single atoms density matrix satisfies rho^2 = rho (condition for pure states)
-        np.testing.assert_array_almost_equal(np.matmul(
-            random_atoms.density_matrices, random_atoms.density_matrices), random_atoms.density_matrices)
+        atomic_ensemble_test_function(random_atoms)
+
+    # Test AtomicEnsemble from create_random_ensemble_from_gaussian_distribution method
+    pos_params = {
+        'mean_x': 1.0,
+        'std_x': 1.0,
+        'mean_y': 1.0,
+        'std_y': 1.0,
+        'mean_z': 1.0,
+        'std_z': 1.0,
+    }
+    vel_params = {
+        'mean_vx': 1.0,
+        'std_vx': ais.convert.vel_from_temp(3.0e-6),
+        'mean_vy': 1.0,
+        'std_vy': ais.convert.vel_from_temp(3.0e-6),
+        'mean_vz': 1.0,
+        'std_vz': ais.convert.vel_from_temp(0*.2e-6),
+    }
+    atoms = ais.create_random_ensemble_from_gaussian_distribution(
+        pos_params, vel_params, n_atom, state_kets=norm_random_kets )
+    atomic_ensemble_test_function(atoms)
