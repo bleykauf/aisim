Fix wrong values for the effective wave vector and conversion from phase error to gravity value. Fix wavefront aberration example notebook.

aisim/convert.py
@@ -107,12 +107,12 @@ def pol2cart(pol):
     return np.array([x, y, z]).T
 
 
-def phase_to_grav(phase, T, keff):
+def phase_error_to_grav(phase, T, keff):
     """
-    Convert a phase shift to gravitational acceleration.
+    Convert a phase error to gravitational acceleration.
 
     Takes the phase shift measured in a Mach Zehnder atom interferometer and converts it
-    to the corresponding gravtional accleration.
+    to the corresponding relative gravitional accleration.
 
     Parameters
     ----------
@@ -126,6 +126,6 @@ def phase_to_grav(phase, T, keff):
     Returns
     -------
     float :
-        gravitational acceleration in m/s²
+        gravitational acceleration in multiples of g
     """
     return phase / keff / (T**2)

tests/wf_test.py
@@ -63,7 +63,7 @@ for r_det in r_dets:
 
     awfs.append(weighted_awf)
 
-grav = ais.phase_to_grav(np.angle(awfs), T=260e-3, keff=1.610574779769e6)
+grav = ais.phase_error_to_grav(np.angle(awfs), T=260e-3, keff=1.610574779769e7)
 
 # test that simulation and data fit reasonably
 assert np.sqrt(np.sum((grav - grav_data) ** 2)) < 1e-8