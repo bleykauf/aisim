Removed buggy grid based approach for ensembles and fix previously wrong wavefront aberration calculation.
* correct wavefront data is added
* `k_eff` was fixed
* random samples instead of fixed grid

aisim/atoms.py
@@ -3,8 +3,6 @@
 import numpy as np
 import scipy.linalg as splin
 
-from . import convert
-
 
 class AtomicEnsemble:
     """
@@ -12,8 +10,6 @@ class AtomicEnsemble:
 
     Each atom is is defined by its phase space vector (x0, y0, z0, vx, vy, vz) at time
     t=0. From this phase space vector the position at later times can be calculated.
-    Optionally, weights can be added for each atom in the ensemble. Slicing along the
-    axis of the n atoms is supported.
 
     Parameters
     ----------
@@ -30,8 +26,6 @@ class AtomicEnsemble:
     time : float, optional
         the initial time (default 0) when the phase space and state vectors are
         initialized
-    weights : 1darray , optional
-        Optional weights for each of the n atoms in the ensemble
 
     Attributes
     ----------
@@ -47,7 +41,7 @@ class AtomicEnsemble:
 
     """
 
-    def __init__(self, phase_space_vectors, state_kets=[1, 0], time=0, weights=None):
+    def __init__(self, phase_space_vectors, state_kets=[1, 0], time=0):
         assert phase_space_vectors.shape[1] == 6
         self.phase_space_vectors = phase_space_vectors
         self.state_kets = state_kets
@@ -56,37 +50,28 @@ class AtomicEnsemble:
         self.initial_position = self.phase_space_vectors[:, 0:3]
         self.initial_velocity = self.phase_space_vectors[:, 3:6]
 
-        if weights is None:
-            weights = np.ones(len(self))  # unity weight for each atom
-        else:
-            assert len(weights) == self.phase_space_vectors.shape[0]
-        self.weights = weights
-
     def __getitem__(self, key):
         """Select  certain atoms "from the ensemble.
 
-        Parameters
+            Parameters
         ----------
-        key : int or slice or bool map
-            for example 2, 1:15 or a boolean map
+            key : int or slice or bool map
+                for example 2, 1:15 or a boolean map
 
-        Returns
-        -------
-        new_instance : AtomicEnsemble
-            a new instance of the atomic ensemble only containing the selected atoms
+            Returns
+            -------
+            new_instance : AtomicEnsemble
+                a new instance of the atomic ensemble only containing the selected atoms
         """
         phase_space_vectors = self.phase_space_vectors[key][:]
         state_kets = self.state_kets[key][:]
-        weights = self.weights[key]
         if isinstance(key, int):
-            # ratain correct shape in case of only one atom is selected
+            # retain correct shape in case of only one atom is selected
             phase_space_vectors = phase_space_vectors.reshape(1, 6)
             state_kets = state_kets.reshape(1, len(state_kets))
-            weights = weights.reshape(1, 1)
         new_instance = AtomicEnsemble(
             phase_space_vectors=phase_space_vectors,
             state_kets=state_kets,
-            weights=weights,
         )
         return new_instance
 
@@ -277,123 +262,6 @@ def create_random_ensemble_from_gaussian_distribution(
     )
     ensemble = AtomicEnsemble(phase_space_vectors, **kwargs)
     return ensemble
-
-
-def create_ensemble_from_grids(pos_params, vel_params, **kwargs):
-    """
-    Create an atomic ensemble from evenly spaced position and velocity grids.
-
-    The resulting position and velocity grids are evenly spaced on polar coordinates.
-
-    Parameters
-    ----------
-    pos_params, vel_params : dict
-        Dictionary containing the parameters determining the position and velocity
-        distributions of the atomic ensemble. They each have to contain the arguments
-        described in the docstring of `make_grid`, i.e. `std_rho`, `std_z` (required),
-        `n_rho`, `n_theta`, `n_z`, `m_std_rho`, `m_std_z`, `weight`, optional.
-    **kwargs :
-        Optional keyworded arguments passed to `AtomicEnsemble`
-
-    Returns
-    -------
-    ensemble : AtomicEnsemble
-        Atomic ensemble contains all possible combinations of the position and velocity
-        grid as phase space vectors. They vectors are weighted according to the combined
-        (multiplied) weights of the respective position and velocity distributions
-        according to the `weight` arguments in `pos_params` and `vel_params`
-    """
-    pos_grid, pos_weights = make_grid(**pos_params)
-    vel_grid, vel_weights = make_grid(**vel_params)
-    grid = combine_grids(pos_grid, vel_grid)
-    weights = combine_weights(pos_weights, vel_weights)
-    ensemble = AtomicEnsemble(grid, weights=weights, **kwargs)
-    return ensemble
-
-
-def make_grid(std_rho, std_z, n_rho=20, n_theta=36, n_z=1, m_std_rho=3, m_std_z=0):
-    """
-    Evenly spaced grid of positions (or velocities) and weights.
-
-    Each of these positions (or velocities) are evenly spaced in polar coordinates and
-    weighted according to a gaussian distribution.
-
-    Parameters
-    ----------
-    std_rho, std_sigma : float
-        1/e radius of the position or velocity distribution.
-    n_rho, n_theta, n_z : int
-        number of grid points per standard deviation along rho and z direction and
-        total number of points along theta, respectively
-    m_std_rho, m_std_z : int
-        number of standard deviations for the rho and z distribution
-
-    Returns
-    -------
-    grid : n × 3 array
-        Grid of n vectors in carthesian coordinates  (x, y, z). In polar coordinates,
-        the grid has this form:
-        [[dRho, 0, -m_std_z*sigma_z/2]
-        [dRho, dTheta, ...]
-        [dRho   , 2*dTheta, ...]
-        [...    , ..., 0]
-        [dRho   , <2*pi, ...]
-        [2*dRho , 0, ...]
-        [2*dRho , dTheta, ...]
-        [...    , ..., ...
-        [m_std_rho*sigma_rho , <2*pi, +m_std_z*sigma_z/2]]
-    weights : 1 × n array
-        weights for each vector in the grid
-    """
-    rhos = np.linspace(0, m_std_rho * std_rho, n_rho)
-    thetas = np.linspace(0, 2 * np.pi, n_theta)
-    zs = np.linspace(-m_std_z * std_z / 2, m_std_z * std_z / 2, max(n_z * m_std_z, 1))
-    grid = np.array(np.meshgrid(rhos, thetas, zs)).T.reshape(-1, 3)
-    # get weights before converting to carthesian coordinates
-    weights = np.exp(-grid[:, 0] ** 2 / (2 * std_rho**2))
-    if std_z != 0:
-        # check if distribution is 2d to avoid divide by 0
-        weights = weights * np.exp(-grid[:, 2] ** 2 / (2 * std_z**2))
-    grid = convert.pol2cart(grid)
-    return grid, weights
-
-
-def combine_grids(pos, vel):
-    """
-    Combine a position and velocity grid into an array of phase space vectors.
-
-    The resulting array contains (x, y, z, vx, vy, vz).
-
-    Parameters
-    ----------
-    pos, vel : n, m × 3 array
-        position and velocity grids as generated by `make_grid`
-
-    Returns
-    -------
-    phase_space_vectors : n * m × 1 array
-    """
-    # FIXME: replace with faster version, for example based on meshgrid
-    phase_space_vectors = np.array(
-        [np.array((p, v)).flatten() for p in pos for v in vel]
-    )
-    return phase_space_vectors
-
-
-def combine_weights(pos_weights, vel_weights):
-    """
-    Combine the weights of a position and velocity grid.
-
-    Complements `_combine_grids`.
-
-    Parameters
-    ----------
-    pos_weights, vel_weights : n × 1 array
-        weights of a position and velocity grids.
-
-    """
-    # FIXME: replace with faster version, for example based on meshgrid
-    return np.array([p * v for p in pos_weights for v in vel_weights])
 
 
 def _fidelity(rho_a, rho_b):

tests/beam_test.py
@@ -10,15 +10,17 @@ def test_wavefront():
     wf = ais.gen_wavefront(1)
     wf.plot()
     wf.plot_coeff()
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
     wf.plot(ax=ax)
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
     wf.plot_coeff(ax=ax)
 
-    with pytest.raises(ValueError):
-        rho = np.array([1.0, 1.1])
-        theta = np.array([0, 0])
-        ais.Wavefront.zern_iso(rho, theta, coeff=wf.coeff, r_beam=1)
+    rho = np.array([1.0, 1.1])
+    theta = np.array([0, 0])
+
+    res = ais.Wavefront.zern_iso(rho, theta, coeff=wf.coeff, r_beam=1)
+    assert not np.isnan(res[0])
+    assert np.isnan(res[1])
 
 
 def test_wavevectors():

tests/wf_test.py
@@ -1,40 +1,44 @@
+import json
+
 import numpy as np
-import pytest  # noqa
 
 import aisim as ais
 
-data = np.loadtxt("docs/examples/data/wf_grav_data.csv", skiprows=1, delimiter=",")
-r_dets = data[:, 0]
-grav_data = data[:, 1]
+data = json.load(open("docs/examples/data/experimental_data.json"))
+r_dets = np.array(data["r_det"])
+grav_data = np.array(data["g"])
 
-coeff_window = np.loadtxt("docs/examples/data/wf_window.txt")
 
-wf = ais.Wavefront(11e-3, coeff_window)
+coeff_window = np.loadtxt("docs/examples/data/wavefront.txt")
+wf = ais.Wavefront(10.91e-3, coeff_window)
 wf.coeff[0:2] = 0  # remove piston, tip and tilt
 
 pos_params = {
-    "std_rho": 3.0e-3,  # cloud radius in m
-    "std_z": 0,  # ignore z dimension, its not relevant here
-    "n_rho": 20,  # within each standard deviation of the distribution
-    "n_theta": 36,  # using a resolution of 10°
-    "n_z": 1,  # use one value for the distribution along z
-    "m_std_rho": 3,  # use 3 standard deviations of the distribution
-    "m_std_z": 0,  # ignore z dimension, its not relevant here
+    "mean_x": 0.0,
+    "std_x": 3.0e-3,  # cloud radius in m
+    "mean_y": 0.0,
+    "std_y": 3.0e-3,  # cloud radius in m
+    "mean_z": 0.0,
+    "std_z": 0.0,  # ignore z dimension, its not relevant here
 }
-
 vel_params = {
-    # velocity spread in m/s from a temperature of 3 uK
-    "std_rho": ais.vel_from_temp(3e-6),
-    "std_z": 0,  # ignore z dimension, its not relevant here
-    # within each standard deviation of the distribution we use 20 points
-    "n_rho": 20,
-    "n_theta": 36,  # using a resolution of 10°
-    "n_z": 1,  # use one value for the distribution along z
-    "m_std_rho": 3,  # use 3 standard deviations of the distribution
-    "m_std_z": 0,  # ignore z dimension, its not relevant here
+    "mean_vx": 0.0,
+    "std_vx": ais.convert.vel_from_temp(
+        3.5e-6
+    ),  # cloud velocity spread in m/s at tempearture of 3 uK
+    "mean_vy": 0.0,
+    "std_vy": ais.convert.vel_from_temp(
+        3.5e-6
+    ),  # cloud velocity spread in m/s at tempearture of 3 uK
+    "mean_vz": 0.0,
+    "std_vz": ais.convert.vel_from_temp(
+        160e-9
+    ),  # after velocity selection, velocity in z direction is 160 nK
 }
 
-atoms = ais.create_ensemble_from_grids(pos_params, vel_params)
+atoms = ais.create_random_ensemble_from_gaussian_distribution(
+    pos_params, vel_params, int(1e5), state_kets=[0, 1], seed=1
+)
 
 t_det = 778e-3  # time of the detection in s
 
@@ -50,20 +54,16 @@ for r_det in r_dets:
 
     det_atoms = det.detected_atoms(atoms)
 
-    # calculate the imprinted phase for each "test atom" at each pulse.
-    # This is the computationally heavy part
+    # calculate the imprinted phase for each "test atom" at each pulse. This is the computationally heavy part
     phi1 = 2 * np.pi * wf.get_value(det_atoms.calc_position(t1))
     phi2 = 2 * np.pi * wf.get_value(det_atoms.calc_position(t2))
     phi3 = 2 * np.pi * wf.get_value(det_atoms.calc_position(t3))
 
     # calculate a complex amplitude factor for the Mach-Zehnder sequence and
     # weight their contribution to the signal
-    awf = np.exp(1j * (phi1 - 2 * phi2 + phi3))
-    weighted_awf = np.sum(det_atoms.weights * awf) / np.sum(det_atoms.weights)
+    awfs.append(np.nanmean(np.exp(1j * (phi1 - 2 * phi2 + phi3))))
 
-    awfs.append(weighted_awf)
-
-grav = ais.phase_error_to_grav(np.angle(awfs), T=260e-3, keff=1.610574779769e7)
+grav = 2 * ais.phase_error_to_grav(np.angle(awfs), T=260e-3, keff=1.610574779769e7)
 
 # test that simulation and data fit reasonably
-assert np.sqrt(np.sum((grav - grav_data) ** 2)) < 1e-8
+assert np.sqrt(np.sum((grav - grav_data) ** 2)) < 1.2e-8